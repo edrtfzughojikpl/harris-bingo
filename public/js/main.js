2d54803d2fe7ce0374f1df0c9f8e98feb6cfdc8b
0x6a6f <hamsterturbo@gmx.de>
2021-04-23 23:22:56 +0200

    created bingo

---
+++ public/js/main.js
@@ -0,0 +1,174 @@
+var decodeHtmlEntity = function (x) {
+    return x.replace(/&#(\d+);/g, function (match, dec) {
+        return String.fromCharCode(dec);
+    });
+};
+
+bingos = JSON.parse(decodeHtmlEntity(bingos));
+
+const boxes = document.querySelectorAll('.bingo-item');
+
+var historyItems = [];
+var historyId = [];
+
+// var bingos = [];
+
+var bingoHash = [];
+
+const getFontSize = (textLength) => {
+    const baseSize = 22;
+    if (textLength >= baseSize) {
+        textLength = baseSize - 2;
+    }
+    const fontSize = baseSize - textLength / 1.9;
+    return `${fontSize}px`;
+}
+
+function undoLast() {
+    if (historyItems.length < 1) return;
+    historyItems[historyItems.length - 1].classList.remove('checked');
+    historyItems.pop();
+    historyId.pop();
+    setCookie("history", historyId);
+}
+
+function reset() {
+    if (historyItems.length < 1) return;
+    historyItems.forEach(e => e.classList.remove('checked'));
+    historyItems = [];
+    historyId = [];
+    setCookie("history", historyId);
+}
+
+function getNew() {
+    setCookie("history", []);
+    setCookie("bingoHash", []);
+    reset();
+    bingoHash = [];
+    makeBingo();
+}
+
+
+function getRandomBingo() {
+    const roundNumber = Math.floor(Math.random() * (bingosCopy.length - 1) + 1);
+    var bingoItem = bingosCopy[roundNumber - 1];
+    bingoHash.push(roundNumber);
+    bingosCopy.splice(roundNumber - 1, 1);
+    return bingoItem;
+}
+
+function setCookie(cname, cvalue, exdays = 365) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function getElementIndex(node) {
+    var index = 0;
+    while ((node = node.previousElementSibling)) {
+        index++;
+    }
+    return index;
+}
+
+var empty = true;
+var bingosCopy;
+
+function makeBingo() {
+    bingoHash = (getCookie("bingoHash") != "") ? getCookie("bingoHash").split(",") : [];
+    bingoHash.forEach((hash, index) => {
+        bingoHash[index] = parseInt(hash);
+    });
+    if (bingoHash[0] == 0) bingoHash = [];
+    historyId = (getCookie("history") != "") ? getCookie("history").split(",") : [];
+    historyId.forEach((number, index) => {
+        historyId[index] = parseInt(number);
+        boxes[number].classList.add('checked');
+        historyItems.push(boxes[number]);
+    });
+    if (bingoHash == 0) {
+        empty = true;
+    } else {
+        empty = false;
+    }
+    bingosCopy = [...bingos];
+    boxes.forEach((box, index) => {
+        box.textContent = (empty) ? getRandomBingo().replace(/(&#34;)/gm, "").replace("[", "").replace("]", "") : bingosCopy[bingoHash[index] - 1].replace(/(&#34;)/gm, "").replace("[", "").replace("]", "");
+        if (!empty) bingosCopy.splice(bingoHash[index] - 1, 1);
+        box.style.fontSize = getFontSize(box.textContent.length);
+    });
+    setCookie("bingoHash", bingoHash);
+}
+
+boxes.forEach(box => {
+    box.addEventListener('click', (e) => {
+        if (e.target.classList.contains('checked')) return;
+        e.target.classList.add('checked');
+        historyItems.push(e.target);
+        historyId.push(getElementIndex(e.target));
+        setCookie("history", historyId);
+    });
+});
+
+makeBingo();
+
+function submit() {
+    var username = prompt("Please enter your name", "");
+    getNew();
+    socket.send(JSON.stringify({
+        username,
+        historyId,
+        bingoHash
+    }))
+}
+
+function makeNot(username) {
+    const div = document.createElement("div");
+    const text = document.createElement("div");
+    text.className = "notify__text";
+    var strong = document.createElement("strong");
+    var em = document.createElement('em')
+    em.textContent = username
+    strong.appendChild(em)
+    text.appendChild(strong);
+    var span = document.createElement('span');
+    span.textContent = "just completed a Bingo!";
+    span.style.marginLeft = "5px";
+    text.appendChild(span);
+    div.className = "notify notify--type-1 notify--success";
+    div.appendChild(text);
+    return div;
+}
+
+const not_cont = document.querySelector('.notifications-container');
+socket.on('chat message', function (msg) {
+    var not = makeNot(msg)
+    not_cont.append(not);
+    setTimeout(() => {
+        not.remove();
+    }, 5 * 1000);
+    /*
+    <div class="notify notify--type-1 notify--success">
+        <div class="notify-content">
+            <!-- <div class="notify__title">New Bingo Filled</div> -->
+            <div class="notify__text"><strong><em>Jo</em></strong><span style="margin-left: 5px;">just completed a Bingo!</span></div>
+        </div>
+    </div>
+    */
+});
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const port = process.env.PORT | 80;
+
+const Bingos = [
+  'Harris gibt eine Information in den Funk',
+  'Beschwerde über PD-04',
+  'DE- ESKALATION',
+  'Mehr als 10 Streifen unterwegs',
+  'Harris wird auf seinen Hut angesprochen',
+  'Marshals verhalten sich fragwürdig',
+  'Harris wird ignoriert',
+  'Jemand fragt nach PD-Merch (Polizeiautos, Kluis, etc.)',
+  'Reznikowa im Dienst',
+  'Harris wechselt die Uniform',
+  'Harris drückt einen Anrufer weg',
+  'Harris auf Streife',
+  'O\'Hara erzählt von seiner Frau',
+  'Denton ist nicht erreichbar',
+  'Funk stört',
+  'Jemand wird befördert',
+  'Rekrut wird eingestellt',
+  'Schlange vorm Büro (mind. 3 Personen)',
+  'Dauerfunk',
+  'N.O.O.S.E. (Blackout)',
+  '10-43',
+  '10-99',
+  'Keith',
+  'Hall',
+  'Smith'
+];
+const fs = require('fs');
+io.on('connection', (socket) => {
+  socket.on('message', (data) => {
+    fs.appendFileSync('completedBingos.txt', data+"\n");
+    socket.broadcast.emit('chat message', JSON.parse(data).username);
+  })
+});
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('pages/index', {
+    Bingos: JSON.stringify(Bingos)
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// let data = '01020304050607080910111213141516171819202122232425';
+// let buff = new Buffer(data);
+// let base64data = buff.toString('base64');
+// console.log('"' + data + '" converted to Base64 is "' + base64data + '"');
+
+//  data = base64data;
+//  buff = new Buffer(data, 'base64');
+// let text = buff.toString('ascii');
+// console.log('"' + data + '" converted from Base64 to ASCII is "' + text + '"');
